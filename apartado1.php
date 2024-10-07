f8bf429e946fa9aac5661fe6cbf307dd82f8c46f
dwcs-code-2425 <dwcs.code.2425@gmail.com>
2024-10-07 12:46:06 +0200

    Update apartado1.php

--- apartado1.php
+++ apartado1.php
@@ -1,32 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+const PAGO_HORA_EXTRA  = 12.5;
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Apartado 1 A2.3</title>
-</head>
+$horas_extra = array(0, 0, 1, 0.5, 2, 0, 0);
 
-<body>
-    <?php
+$suma = array_sum($horas_extra);
+//Outra opción
+// $suma = 0;
+// foreach ($horas_extra as $v) {
+//     $suma += $v;
+// }
 
-    const PAGO_HORA_EXTRA = 12.5;
+$salario_extra = $suma * PAGO_HORA_EXTRA;
 
-    $horas_extra_semana = [
-        3.5, 4, 0, 0, 0, 2, 7
-    ];
-
-    $acumulador = 0;
-    for ($i = 0; $i < count($horas_extra_semana); $i++) {
-        // $acumulador+=  $horas_extra_semana[$i] * PAGO_HORA_EXTRA;
-        $acumulador += $horas_extra_semana[$i];
-    }
-
-    $acumulador *= PAGO_HORA_EXTRA;
-
-    echo "<p> El pago de horas extra es: $acumulador </p>";
-    ?>
-
-</body>
-
-</html>
+echo "O salario extra é $salario_extra";
